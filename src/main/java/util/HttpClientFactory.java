65cacce709e11cd61dc12a50e8e79539ff926824
bingoo <bingoo.huang@gmail.com>
2019-11-04 23:21:43 +0800

    refactoring

--- src/main/java/util/HttpClientFactory.java
+++ src/main/java/util/HttpClientFactory.java
@@ -1,27 +1,23 @@
 package util;
 
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.apache.http.HttpEntityEnclosingRequest;
-import org.apache.http.HttpRequest;
 import org.apache.http.NoHttpResponseException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.HttpRequestRetryHandler;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
 import org.apache.http.config.SocketConfig;
 import org.apache.http.conn.ConnectTimeoutException;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
 import org.apache.http.conn.socket.PlainConnectionSocketFactory;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.protocol.HttpContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.net.ssl.SSLException;
 import javax.net.ssl.SSLHandshakeException;
@@ -29,107 +25,106 @@ import java.io.IOException;
 import java.io.InterruptedIOException;
 import java.net.ConnectException;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
+import java.util.LinkedHashMap;
 
-public class HttpClientFactory {
+@Slf4j
+public abstract class HttpClientFactory {
 
-    private static Logger log = LoggerFactory.getLogger(HttpClientFactory.class);
+  /** 请求超时时间 */
+  private static final int connectionRequestTimeout = 500; // 500毫秒
+  /** 建立连接超时时间 */
+  private static final int connectionTimeoutTime = 500; // 500毫秒
+  /** 读取超时 */
+  private static final int soTimeoutTime = 500; // 500 毫秒
 
-    /**
-     * 请求超时时间
-     */
-    private static final Integer connectionRequestTimeout = 500;// 500毫秒
-    /**
-     * 建立连接超时时间
-     */
-    private static final Integer connectionTimeoutTime = 500;// 500毫秒
-    /**
-     * 读取超时
-     */
-    private static final Integer soTimeoutTime = 500;// 500 毫秒
+  private static final int maxTotal = 300;
+  private static final int defaultMaxPerRoute = 300;
 
-    private static final int MaxTotal = 300;
-    private static final int DefaultMaxPerRoute = 300;
-    private static HttpClientBuilder httpBulder = null;
-    static final String CONTENT_TYPE = "Content-Type";
-    static final String BASP_CONTENT_TYPE = "application/Json";
+  private static HttpClientBuilder httpBuilder = createHttpClientBuilderWithLogEx();
 
-    static {
-        init();
+  public static HttpClient getClient() {
+    return httpBuilder.build();
+  }
+
+  private static HttpClientBuilder createHttpClientBuilderWithLogEx() {
+    try {
+      return createHttpClientBuilder();
+    } catch (Exception e) {
+      log.error("httpClient初始化异常了！", e);
+      throw new RuntimeException("初始化HttpClient连接池失败了！", e);
     }
+  }
 
-    public static HttpClient getClient() {
-        CloseableHttpClient httpClient = httpBulder.build();
-        return httpClient;
-    }
+  private static HttpClientBuilder createHttpClientBuilder() {
+    return HttpClients.custom()
+        .setConnectionManager(getPoolingConnectionManager())
+        .setDefaultRequestConfig(getRequestConfig())
+        .setRetryHandler(getHttpRequestRetryHandler());
+  }
 
-    private static void init() {
-        try {
-            ConnectionSocketFactory plainsf = PlainConnectionSocketFactory.getSocketFactory();
-            LayeredConnectionSocketFactory sslsf = SSLConnectionSocketFactory.getSocketFactory();
+  private static HttpRequestRetryHandler getHttpRequestRetryHandler() {
+    val retryClasses = new LinkedHashMap<Class<? extends IOException>, Boolean>();
+    retryClasses.put(NoHttpResponseException.class, true); // 如果服务器丢掉了连接，那么就重试
+    retryClasses.put(SSLHandshakeException.class, false); // 不要重试SSL握手异常
+    retryClasses.put(SocketTimeoutException.class, false); // socket超时
+    retryClasses.put(ConnectTimeoutException.class, false); // 连接超时
+    retryClasses.put(InterruptedIOException.class, true); // 超时
+    retryClasses.put(UnknownHostException.class, true); // 目标服务器不可达
+    retryClasses.put(SSLException.class, false); // ssl握手异常
+    retryClasses.put(ConnectException.class, true);
+    retryClasses.put(SocketException.class, true);
 
-            Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
-                    .register("http", plainsf).register("https", sslsf).build();
+    // 请求重试处理
+    return (exception, executionCount, context) -> {
+      log.warn("HttpRequestRetryHandler executionCount:{}", executionCount, exception);
 
-            PoolingHttpClientConnectionManager poolConnManager = new PoolingHttpClientConnectionManager(registry);
-            poolConnManager.setMaxTotal(MaxTotal);
-            // 将每个路由基础的连接最大 单个路由 跟总的一致
-            poolConnManager.setDefaultMaxPerRoute(DefaultMaxPerRoute);
+      if (executionCount >= 5) { // 如果已经重试了5次，就放弃
+        return false;
+      }
 
-            SocketConfig socketConfig = SocketConfig.custom().setSoTimeout(soTimeoutTime).build();
-            poolConnManager.setDefaultSocketConfig(socketConfig);
+      if (retryClasses.containsKey(exception.getClass())) {
+        return retryClasses.get(exception.getClass());
+      }
 
-            RequestConfig requestConfig = RequestConfig.custom().setConnectionRequestTimeout(connectionRequestTimeout)
-                    .setConnectTimeout(connectionTimeoutTime).setSocketTimeout(soTimeoutTime).build();
-
-            // 请求重试处理
-            HttpRequestRetryHandler httpRequestRetryHandler = new HttpRequestRetryHandler() {
-                public boolean retryRequest(IOException exception, int executionCount, HttpContext context) {
-                    if (executionCount >= 5) {// 如果已经重试了5次，就放弃
-                        return false;
-                    }
-                    if (exception instanceof NoHttpResponseException) {// 如果服务器丢掉了连接，那么就重试
-                        return true;
-                    }
-                    if (exception instanceof SSLHandshakeException) {// 不要重试SSL握手异常
-                        return false;
-                    }
-                    if (exception instanceof InterruptedIOException) {// 超时
-                        return true;
-                    }
-                    if (exception instanceof UnknownHostException) {// 目标服务器不可达
-                        return true;
-                    }
-                    if (exception instanceof ConnectTimeoutException) {// 连接被拒绝
-                        return false;
-                    }
-                    if (exception instanceof SSLException) {// ssl握手异常
-                        return false;
-                    }
-                    if (exception instanceof ConnectException) {
-                        return true;
-                    }
-                    if (exception instanceof SocketException) {
-                        return true;
-                    }
-
-                    HttpClientContext clientContext = HttpClientContext.adapt(context);
-                    HttpRequest request = clientContext.getRequest();
-                    // 如果请求是幂等的，就再次尝试
-                    if (!(request instanceof HttpEntityEnclosingRequest)) {
-                        return true;
-                    }
-                    return false;
-                }
-            };
-
-            httpBulder = HttpClients.custom().setConnectionManager(poolConnManager).setDefaultRequestConfig(requestConfig)
-                    .setRetryHandler(httpRequestRetryHandler);
-
-        } catch (Exception e) {
-            log.error("httpClient初始化异常了！");
-            throw new RuntimeException("初始化HttpClient连接池失败了！", e);
+      for (val entry : retryClasses.entrySet()) {
+        if (entry.getKey().isInstance(exception)) {
+          return entry.getValue();
         }
-    }
+      }
 
+      return isIdempotent(context); // 如果请求是幂等的，就再次尝试
+    };
+  }
+
+  private static PoolingHttpClientConnectionManager getPoolingConnectionManager() {
+    val registry =
+        RegistryBuilder.<ConnectionSocketFactory>create()
+            .register("http", PlainConnectionSocketFactory.getSocketFactory())
+            .register("https", SSLConnectionSocketFactory.getSocketFactory())
+            .build();
+
+    val poolConnManager = new PoolingHttpClientConnectionManager(registry);
+    poolConnManager.setMaxTotal(maxTotal);
+    // 将每个路由基础的连接最大 单个路由 跟总的一致
+    poolConnManager.setDefaultMaxPerRoute(defaultMaxPerRoute);
+
+    val socketConfig = SocketConfig.custom().setSoTimeout(soTimeoutTime).build();
+    poolConnManager.setDefaultSocketConfig(socketConfig);
+    return poolConnManager;
+  }
+
+  private static RequestConfig getRequestConfig() {
+    return RequestConfig.custom()
+        .setConnectionRequestTimeout(connectionRequestTimeout)
+        .setConnectTimeout(connectionTimeoutTime)
+        .setSocketTimeout(soTimeoutTime)
+        .build();
+  }
+
+  private static boolean isIdempotent(HttpContext context) {
+    val request = HttpClientContext.adapt(context).getRequest();
+    return !(request instanceof HttpEntityEnclosingRequest);
+  }
 }
--- src/main/java/util/HttpUtils.java
+++ src/main/java/util/HttpUtils.java
@@ -1,151 +1,118 @@
 package util;
 
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.apache.http.Consts;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.message.AbstractHttpMessage;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.net.URI;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Map;
 
+import static util.HttpClientFactory.getClient;
+
+@Slf4j
 public class HttpUtils {
+  public static String get(String url, Map<String, String> paramMap, Map<String, String> headMap) {
+    HttpGet httpGet = new HttpGet(url);
 
-    private static Logger log = LoggerFactory.getLogger(HttpUtils.class);
+    if (paramMap != null && !paramMap.isEmpty()) {
+      val params = new ArrayList<NameValuePair>();
+      for (val entry : paramMap.entrySet()) {
+        Object key = entry.getKey();
+        Object value = entry.getValue();
+        params.add(new BasicNameValuePair(key.toString(), value.toString()));
+      }
 
-
-    public static String excuteGet(String url, Map<String, String> paramMap,
-                                   Map<String, String> headMap, String encoding) {
-        HttpGet httpGet = new HttpGet(url);
-
-        List<NameValuePair> params = null;
-        if (paramMap != null && !paramMap.isEmpty()) {
-            params = new ArrayList<NameValuePair>();
-            Set<String> set = paramMap.keySet();
-            Iterator<String> it = set.iterator();
-            while (it.hasNext()) {
-                Object key = it.next();
-                Object value = paramMap.get(key);
-                NameValuePair nameValuePair = new BasicNameValuePair(key.toString(), value.toString());
-                params.add(nameValuePair);
-            }
-            String str;
-            try {
-                str = EntityUtils.toString(new UrlEncodedFormEntity(params, encoding), encoding);
-                httpGet.setURI(new URI(httpGet.getURI().toString() + "?" + str));
-            } catch (Exception e) {
-                log.error("get请求 参数设置异常", e);
-                throw new RuntimeException("get请求 参数设置异常", e);
-            }
-        } else {
-            log.warn("paramMap is null");
-        }
-
-        //header处理
-        if (headMap != null && !headMap.isEmpty()) {
-            Set<String> set = headMap.keySet();
-            Iterator<String> it = set.iterator();
-            while (it.hasNext()) {
-                String key = it.next();
-                String value = headMap.get(key);
-                httpGet.setHeader(key, value);
-            }
-        }
-
-        String resultStr;
-        try {
-            BasicResponseHandler basicResponseHandler = new BasicResponseHandler();
-            resultStr = HttpClientFactory.getClient().execute(httpGet, basicResponseHandler);
-        } catch (Exception e) {
-            log.error("get请求异常", e);
-            throw new RuntimeException("get请求异常", e);
-        } finally {
-            if (httpGet != null && !httpGet.isAborted()) {
-                httpGet.abort();
-            }
-        }
-        return resultStr;
+      try {
+        val str = EntityUtils.toString(new UrlEncodedFormEntity(params, "UTF-8"), "UTF-8");
+        httpGet.setURI(new URI(httpGet.getURI().toString() + "?" + str));
+      } catch (Exception e) {
+        log.error("get请求 参数设置异常", e);
+        throw new RuntimeException("get请求 参数设置异常", e);
+      }
+    } else {
+      log.warn("paramMap is null");
     }
 
-    public static String sendPost(String url, String jsonContent) {
-        HttpPost post = new HttpPost(url);
-        post.setEntity(new StringEntity(jsonContent, ContentType.APPLICATION_JSON));
-        post.setHeader(HttpClientFactory.CONTENT_TYPE, HttpClientFactory.BASP_CONTENT_TYPE);
-        String resultStr;
-        try {
-            BasicResponseHandler basicResponseHandler = new BasicResponseHandler();
-            resultStr = HttpClientFactory.getClient().execute(post, basicResponseHandler);
-        } catch (Exception e) {
-            log.error("post jsonbody请求异常", e);
-            throw new RuntimeException("post请求异常", e);
-        } finally {
-            if (post != null && !post.isAborted()) {
-                post.abort();
-            }
-        }
-        return resultStr;
+    // header处理
+    setHeaders(headMap, httpGet);
+
+    return execute(httpGet, "get请求异常");
+  }
+
+  public static String post(String url, String jsonContent) {
+    HttpPost post = new HttpPost(url);
+    val contentType = ContentType.create("application/json", Consts.UTF_8);
+    post.setEntity(new StringEntity(jsonContent, contentType));
+    post.setHeader("Content-Type", "application/Json");
+    return execute(post, "post jsonbody请求异常");
+  }
+
+  public static String postForm(
+      String url, Map<String, String> paramMap, Map<String, String> headMap) {
+    HttpPost post = new HttpPost(url);
+
+    // 参数处理
+    if (paramMap != null && !paramMap.isEmpty()) {
+      String paramStr = "";
+      for (val entry : paramMap.entrySet()) {
+        paramStr += "&" + entry.getKey() + "=" + entry.getValue();
+      }
+      // 构造最简单的字符串数据
+      StringEntity reqEntity;
+      try {
+        reqEntity = new StringEntity(paramStr.substring(1), "UTF-8");
+      } catch (Exception e) {
+        log.warn("构造参数出错了！", e);
+        return "";
+      }
+      // 设置类型
+      reqEntity.setContentType("application/x-www-form-urlencoded");
+      post.setEntity(reqEntity);
+    } else {
+      log.warn("paramMap is null");
+      return "";
     }
 
-    public static String excutePostForm(String url, Map<String, String> paramMap,
-                                        Map<String, String> headMap, String encoding) {
-        HttpPost post = new HttpPost(url);
+    setHeaders(headMap, post);
 
-        //参数处理
-        if (paramMap != null && !paramMap.isEmpty()) {
-            String paramStr = "";
-            Set<String> set = paramMap.keySet();
-            Iterator<String> it = set.iterator();
-            while (it.hasNext()) {
-                Object key = it.next();
-                Object value = paramMap.get(key);
-                paramStr += "&" + key + "=" + value;
-            }
-            // 构造最简单的字符串数据
-            StringEntity reqEntity = null;
-            try {
-                reqEntity = new StringEntity(paramStr.substring(1), encoding);
-            } catch (Exception e) {
-                log.warn("构造参数出错了！", e);
-                return "";
-            }
-            // 设置类型
-            reqEntity.setContentType("application/x-www-form-urlencoded");
-            post.setEntity(reqEntity);
-        } else {
-            log.warn("paramMap is null");
-            return "";
-        }
-        //header处理
-        if (headMap != null && !headMap.isEmpty()) {
-            Set<String> set = headMap.keySet();
-            Iterator<String> it = set.iterator();
-            while (it.hasNext()) {
-                String key = it.next();
-                String value = headMap.get(key);
-                post.setHeader(key, value);
-            }
-        }
+    return execute(post, "post表单请求异常");
+  }
 
-        String resultStr;
-        try {
-            BasicResponseHandler basicResponseHandler = new BasicResponseHandler();
-            resultStr = HttpClientFactory.getClient().execute(post, basicResponseHandler);
-        } catch (Exception e) {
-            log.error("post表单请求异常", e);
-            throw new RuntimeException("post表单请求异常", e);
-        } finally {
-            if (post != null && !post.isAborted()) {
-                post.abort();
-            }
-        }
-        return resultStr;
+  private static String execute(HttpUriRequest request, String exceptionMsg) {
+    try {
+      return getClient().execute(request, new BasicResponseHandler());
+    } catch (Exception e) {
+      log.error(exceptionMsg, e);
+      throw new RuntimeException(
+          request.getMethod() + " " + request.getURI() + " exception " + exceptionMsg, e);
+    } finally {
+      // To abort an ongoing request, the client can simply use:
+      //
+      // This will make sure that the client doesn't have to consume the entire body of the request
+      // to release the connection:
+      request.abort();
+    }
+  }
+
+  private static void setHeaders(Map<String, String> headMap, AbstractHttpMessage httpMessage) {
+    if (headMap == null) {
+      return;
     }
 
-
+    for (val entry : headMap.entrySet()) {
+      httpMessage.setHeader(entry.getKey(), entry.getValue());
+    }
+  }
 }
